5c9dba5e53c8835c7aa57365648854ac9af16e7d
AmiFideles <251645@niuitmo.ru>
2024-10-25 05:11:32 +0300

    feature: add seller review service implementations

---
+++ src/main/java/ru/itmo/marketplace/repository/SellerReviewRepository.java
@@ -0,0 +1,18 @@
+package ru.itmo.marketplace.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.itmo.marketplace.entity.SellerReview;
+
+@Repository
+public interface SellerReviewRepository extends JpaRepository<SellerReview, SellerReview.SellerReviewId> {
+    boolean existsByAuthorIdAndSellerId(Long authorId, Long sellerId);
+
+    Page<SellerReview> findByAuthorId(Long authorId, Pageable pageable);
+
+    Page<SellerReview> findBySellerId(Long sellerId, Pageable pageable);
+
+    int deleteByAuthorIdAndSellerId(Long authorId, Long sellerId);
+}
