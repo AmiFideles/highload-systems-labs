06cfa95366a2516e1a1fddcd9275f043b406f9db
wieceslaw <vyache-nov@mail.ru>
2025-01-04 23:09:06 +0300

    feature: added notification service

---
+++ notification-service/src/main/java/ru/itmo/marketplace/service/notification/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package ru.itmo.marketplace.service.notification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.itmo.common.exception.ControllerAdvice;
+import ru.itmo.modules.security.InternalAuthenticationFilter;
+import ru.itmo.modules.security.SecurityModuleConfig;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Import({
+        SecurityModuleConfig.class,
+        ControllerAdvice.class,
+})
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            InternalAuthenticationFilter authenticationFilter
+    ) throws Exception {
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeHttpRequests()
+                .requestMatchers("/error").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/ws**").authenticated()
+                .and()
+
+                .build();
+    }
+}
